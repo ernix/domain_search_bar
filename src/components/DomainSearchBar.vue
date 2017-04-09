7a4082dcb6109a2f084f9f5b3bd774b1a95cdb4e
Shin Kojima <shin@kojima.org>
2017-04-09 21:50:36 +0900

    in progress

--- src/components/DomainSearchBar.vue
+++ src/components/DomainSearchBar.vue
@@ -1,9 +1,16 @@
 <template>
   <div class="domain-search-bar">
-    <form>
-      <input name="domain" type="text" />
-      <button name="submit" type="submit">search</button>
+
+    <form @submit.prevent>
+      <input v-model="tld" name="tld" type="hidden" />
+      <input v-model="next" name="domain" type="text" />
+      <button @click="add" name="submit" type="submit">search</button>
     </form>
+
+    <div v-for="domain in domains" :is="domain.type">
+      <result :tld="domain.tld" :domain="domain.domain" ></result>
+    </div>
+
   </div>
 </template>
 
@@ -18,114 +25,25 @@ export default {
   data () {
     return {
       next: '',
-      list: []
+      tld: '',
+      domains: []
+    }
+  },
+  methods: {
+    add (e) {
+      this.domains.push({
+        domain: this.next,
+        tld: this.tld,
+        type: 'Result'
+      })
+
+      this.next = ''
     }
   }
 }
-/*
-(function ($) {
-  'use strict';
-
-  $(function () {
-    var line = $('#line').html();
-
-    $('#register').submit(function (e) {
-      e.preventDefault();
-
-      var label = punycode.toASCII(this.domain.value.replace(/^\s*|\s*$/, "", "g"));
-      var re_label = /^(.*)\.earth$/i;
-      var m = re_label.exec(label);
-      if (m !== null) {
-        label = m[1];
-      }
-
-      $('#result').prepend(_.template(line)({label: label}));
-
-      $('#result div.unresolve').each(function () {
-        var self = $(this);
-
-        if (label === '') {
-          self.append('<span>Submit a domain</span>');
-          self.removeClass('unresolve');
-          return false;
-        }
-
-        var timer;
-        var domain = self.data('domain');
-        $.ajax({
-          dataType: 'jsonp',
-          async: true,
-          cache: false,
-          url: 'http://domain.earth/domain_search/earth.cgi',
-          jsonp: 'callback',
-          data: { domain: label },
-          beforeSend: function () {
-            var dom = $('<span style="width:4em;">'), dots = '.';
-            timer = setInterval(function () {
-              dots = (dots.length > 2) ? '' : dots + '.';
-              dom.text(dots);
-            }, 500);
-            self.find('span').append(dom);
-          }
-        })
-        .done(function (data) {
-          if (data === null || data.status !== '0') {
-            self
-              .find('span')
-              .addClass('text-warning')
-              .addClass('btn')
-              .html('Unavailable');
-            return true;
-          }
-
-          if (data.hasOwnProperty('premium') && data.premium === '1') {
-            self
-              .find('span')
-              .addClass('text-warning')
-              .addClass('btn')
-              .html('Premium');
-            return true;
-          }
-
-          $('#partners a').each(function () {
-            var uri = $(this).uri();
-            uri.setQuery({'domain': domain});
-          });
-
-          $('#partners').slideDown();
-
-          var text = 'Available!';
-
-          self
-            .find('span')
-            .html(
-              $('<span>').addClass('text-success').addClass('btn')
-                .css('font-size', 'large')
-                .text(text)
-            );
-
-          return true;
-        })
-        .fail(function () {
-          self
-            .find('span')
-            .addClass('text-warning')
-            .addClass('btn')
-            .html('Unavailable');
-          return true;
-        })
-        .always(function () {
-          clearInterval(timer);
-        });
-
-        self.removeClass('unresolve');
-      });
-    });
-  });
-})(jQuery);
-*/
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
+/* TODO: style goes here */
 </style>
--- src/components/Result.vue
+++ src/components/Result.vue
@@ -8,36 +8,51 @@ import punycode from 'punycode'
 
 export default {
   name: 'result',
+  props: ['domain', 'tld'],
   data () {
     return {
-      domain: 'foobar.earth',
       result: undefined
     }
   },
   created () {
+    // TODO: set domain/tld props
+    // TODO: start search domain
+
+    console.log(this.domain)
+    // this.tld =
+    // this.domain = this.label()
   },
   watch: {
+    // TODO: monitor search result
   },
   computed: {
     label () {
-      let label = punycode.toASCII(this.domain.replace(/^\s*|\s*$/, '', 'g'))
-
-      let m = (/^(.*)\.earth$/i).exec(label)
-      if (m !== null) {
-        label = m[1]
+      let trimed = this.domain && this.domain.trim()
+      if (!trimed) {
+        return
       }
 
-      return label
+      let ascii = punycode.toASCII(trimed)
+
+      let re = new RegExp('^(.*)\\.' + this.tld + '$', 'i')
+      let match = re.exec(ascii)
+      if (match !== null) {
+        ascii = match[1]
+      }
+
+      return ascii
     }
   },
   methods: {
     search () {
-      const url = 'http://domain.earth/domain_search/earth.cgi'
+      const url = 'http://203.141.128.87/perl/domain_check/dm_check_domain.cgi'
       this.$jsonp(url, { domain: this.label })
         .then(json => {
+          console.log(json)  // TODO: remove
+          return json !== null && json.status === '0'
         })
         .catch(err => {
-          console.log(err)
+          console.log(err)  // TODO: remove
         })
     }
   }
